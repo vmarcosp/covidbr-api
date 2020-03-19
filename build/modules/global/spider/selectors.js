1aca2f4ec323207b81b73920c692bf9831164220
Marcos <vmarcosp.pereira@gmail.com>
2020-03-19 08:50:53 -0300

    fix: Removing build

--- build/modules/global/spider/selectors.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GLOBAL_COUNTERS = '#maincounter-wrap > .maincounter-number > span';
-exports.COUNTERS = '.col-md-6 .number-table-main';
-//# sourceMappingURL=selectors.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata'
 import './config-aliases'
 import { GraphQLServer } from 'graphql-yoga'
 import { buildSchema } from 'type-graphql'
-import { CountryResolver } from './modules/countries/resolvers'
 import { GlobalResolver } from './modules/global/resolvers'
 import crawlerJob from './modules/global/cronjob'
 
@@ -10,7 +9,7 @@ async function run() {
   await crawlerJob.start()
 
   const schema = await buildSchema({
-    resolvers: [CountryResolver, GlobalResolver],
+    resolvers: [GlobalResolver],
     emitSchemaFile: true
   })
 
