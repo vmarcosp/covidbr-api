3f5faf4231bb78b66e4fabad96304e3896ffeae1
Marcos <vmarcosp.pereira@gmail.com>
2020-03-23 19:03:45 -0300

    fix: Removing filter by state name

--- src/modules/states/resolvers/index.ts
+++ src/modules/states/resolvers/index.ts
@@ -6,12 +6,13 @@ import { StateFilterInput } from '../typeDefs/StateFilterInput'
 @Resolver(() => State)
 export class StateResolver {
   @Query(() => State, { nullable: true })
-  async state (@Arg('filter') filter: StateFilterInput): Promise<State | null> {
-    return StatesCollection.findOne({ uf: filter.uf })
+  async state(@Arg('filter') filter?: StateFilterInput): Promise<State | null> {
+    const query = filter ? { uf: filter.uf } : {}
+    return StatesCollection.findOne(query)
   }
 
   @Query(() => [State], { nullable: true })
-  async states (@Arg('filter') filter: StateFilterInput): Promise<State[]> {
-    return StatesCollection.find({}).filter(({ name }) => name.includes(filter.name || ''))
+  async states(): Promise<State[]> {
+    return StatesCollection.find({})
   }
 }
